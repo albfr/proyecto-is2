b8725ec82b950b73b00ad539b1773f9612517402
Tyckong <114450886+Tyckong@users.noreply.github.com>
2025-04-29 22:13:38 -0400

    Update weeklyRecommendations.jsx

--- src/components/weeklyRecommendations.jsx
+++ src/components/weeklyRecommendations.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DailyRecommendation from "./dailyRecommendation";
 import WeekdaySelector from './weekDaySelector';
 
-import '../assets/DRStyle.css';
+import '../styles/styles.css';
 
 function WeeklyRecommendations(){
     const [currentDay, setCurrentDay] = useState('Lunes')
