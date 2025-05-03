dc37e8a065c32788363a0ce642da2c5b1da7705b
Tyckong <tocontreras2021@udec.cl>
2025-05-02 22:43:02 -0400

    Title of weekday buttons changes according to the current day.

--- src/components/WeekDaySelector.jsx
+++ src/components/WeekDaySelector.jsx
@@ -3,13 +3,18 @@ import DayButton from './DayButton';
 import styles from '@/styles/WeekDaySelector.module.css'
 
 const DAYS_OF_WEEK = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
 function WeekdaySelector({ selectedDay, onDayChange }) {
+  const todayIndex = new Date().getDay();
+  const adjustedTodayIndex = (todayIndex + 6) % 7;
+  const todayDayName = DAYS_OF_WEEK[adjustedTodayIndex];
+
   return (
     <div className={styles.weekday_selector}>
-      {DAYS_OF_WEEK.map((day) => (
+      {DAYS_OF_WEEK.map((day, index) => (
         <DayButton
           key={day}
-          day={day}
+          day={index === adjustedTodayIndex ? 'Hoy' : day}
           isSelected={day === selectedDay}
           onClick={onDayChange}
         />
