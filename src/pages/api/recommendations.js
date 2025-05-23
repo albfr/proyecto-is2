6e93b74b247f89cc6eafde77ecec86276381d96c
Leonardo Lovera <leonard.lover@gmail.com>
2025-05-23 10:22:51 -0400

    feat: now weather and recommendations are on the same object for every date

--- src/pages/api/recommendations.js
+++ src/pages/api/recommendations.js
@@ -26,17 +26,70 @@ export default async function handler(req, res) {
       activities.forEach(activity => {
         dayrecs.push({"name": activity.name, "similarity": cosineSimilarity(day.day, activity)});
       });
-      data.push({"day": day.date, "recommendations": dayrecs});
+      data.push({"date": day.date, "recommendations": dayrecs});
     });
 
-    data.push({"forecastday": forecast.map(f => {
+    const reduced_forecast = forecast.map(f => {
       return {
-          date: f.date,
-          ...f.day
+        date: f.date,
+        ...f.day
       };
-    })});
+    });
 
-    res.status(200).json(data);
+    const recommendations_with_weather = [];
+
+    for (let i = 0; i < data.length; i++) {
+        let matched = false;
+
+        for (let j = 0; j < reduced_forecast.length; j++) {
+            if (data[i].date == reduced_forecast[j].date) {
+                const f_no_date = JSON.parse(JSON.stringify(
+                    reduced_forecast[j]
+                ));
+                delete f_no_date.date;
+
+                recommendations_with_weather.push({
+                    ...data[i],
+                    ...f_no_date
+                });
+
+                matched = true;
+                break;
+            }
+        }
+
+        if (!matched) {
+            recommendations_with_weather.push({
+                ...data[i],
+                "maxtemp_c": undefined,
+                "maxtemp_f": undefined,
+                "mintemp_c": undefined,
+                "mintemp_f": undefined,
+                "avgtemp_c": undefined,
+                "avgtemp_f": undefined,
+                "maxwind_mph": undefined,
+                "maxwind_kph": undefined,
+                "totalprecip_mm": undefined,
+                "totalprecip_in": undefined,
+                "totalsnow_cm": undefined,
+                "avgvis_km": undefined,
+                "avgvis_miles": undefined,
+                "avghumidity": undefined,
+                "daily_will_it_rain": undefined,
+                "daily_chance_of_rain": undefined,
+                "daily_will_it_snow": undefined,
+                "daily_chance_of_snow": undefined,
+                "condition": {
+                  "text": undefined,
+                  "icon": undefined,
+                  "code": undefined
+                },
+                "uv": undefined
+            });
+        }
+    }
+
+    res.status(200).json(recommendations_with_weather);
   }
   catch (err) {
     console.error(err);
