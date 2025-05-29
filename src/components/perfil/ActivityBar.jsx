22d4917dd4d544a4d3e6180cee163a19c73694d0
Luciano <luargomedo2021@inf.udec.cl>
2025-05-28 20:49:27 -0400

    ActivityBar creada (está más fea)

--- src/components/perfil/ActivityBar.jsx
+++ src/components/perfil/ActivityBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from '@/styles/ActivityBar.module.css';
-import { useRouter } from 'next/router';
 
-return (
+function ActivityBar() {
+  return (
     <div className={styles.activitybar}>
-
+      <button className={styles.barButton}>Actividades</button>
+      <button className={styles.barButton}>PlaceHolder 1</button>
+      <button className={styles.barButton}>PlaceHolder 2</button>
     </div>
   );
+}
 
-export default ActivityBar
+export default ActivityBar;
