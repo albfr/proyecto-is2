1c2eaef90c7af3732cb755631697c94681647cf9
clcser <clcser2021@udec.cl>
2025-06-03 21:29:31 -0400

    Profile allows multiple activities

--- src/components/perfil/ActivityBoard.jsx
+++ src/components/perfil/ActivityBoard.jsx
@@ -5,20 +5,77 @@ import ActivityModification from "@/components/perfil/ActivityModification";
 import ProfileWrapper from "@/styles/perfil/ProfileWrapper.module.css";
 
 export default function ActivityBoard() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedActivity, setSelectedActivity] = useState(null);
   
-  const openModal = () => {
-    setIsModalOpen(true);
+  const activities = [
+    {
+      id: 0,
+      owner: "alferrada2021",
+      name: "Salir a trotar",
+      max_temp: 25,
+      min_temp: 10,
+      wind: 20,
+      pressure: 1,
+      precipitation: 0,
+      humidity: 20,
+      cloud: true,
+      visibility: 100,
+      uv: 50,
+    },
+    {
+      id: 1,
+      owner: "lelovera2021",
+      name: "Hacer jardinería",
+      max_temp: 25,
+      min_temp: 15,
+      wind: 25,
+      pressure: 1,
+      precipitation: 0,
+      humidity: 25,
+      cloud: true,
+      visibility: 100,
+      uv: 50,
+    },
+    {
+      id: 2,
+      owner: "josantis2021",
+      name: "Pasear al cachupin",
+      max_temp: 27,
+      min_temp: 10,
+      wind: 20,
+      pressure: 1,
+      precipitation: 0,
+      humidity: 15,
+      cloud: true,
+      visibility: 100,
+      uv: 50,
+    },
+  ];
+
+  const openModal = (activity) => {
+    setSelectedActivity(activity);
   };
   
   const closeModal = () => {
-    setIsModalOpen(false);
+    setSelectedActivity(null);
   };
 
   return (
     <div className={styles.activity_board}>
-      <ActivityCard onClick={openModal}/>
-      <ActivityModification open={isModalOpen} onClose={closeModal} />
+      {activities.map((activity) => (
+        <ActivityCard
+          activity={activity}
+          onClick={() => openModal(activity)}
+        />
+      ))}
+
+      {selectedActivity && (
+        <ActivityModification
+          activity={selectedActivity}
+          open={true}
+          onClose={closeModal}
+        />
+      )}
     </div>
   );
 }
--- src/components/perfil/ActivityCard.jsx
+++ src/components/perfil/ActivityCard.jsx
@@ -4,22 +4,7 @@ import styles from "@/styles/perfil/ActivityCard.module.css";
 
 export default function ActivityCard({ activity, onClick }) {
   const [hovered, setHovered] = useState(false);
-
-  activity = {
-    id_activity: 0,
-    owner: "josantis2021",
-    name: "Correr",
-    max_temp: 25,
-    min_temp: 10,
-    wind: 20,
-    pressure: 1,
-    precipitation: 0,
-    humidity: 20,
-    cloud: true,
-    visibility: 100,
-    uv: 50,
-  };
-  
+ 
   useEffect(() => {
     document.body.style.cursor = hovered ? 'pointer' : 'auto'
   }, [hovered]);
