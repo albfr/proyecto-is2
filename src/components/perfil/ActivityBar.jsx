9c928058552d346079871ba0e62fcbcbdd1e1086
Tyckong <tocontreras2021@udec.cl>
2025-07-04 23:10:06 -0400

    Added input field to receive weather notifications and submit button.

--- src/components/perfil/ActivityBar.jsx
+++ src/components/perfil/ActivityBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/styles/perfil/ActivityBar.module.css';
 
 function ActivityBar() {
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+
   return (
     <div className={styles.activitybar}>
       <button className={styles.barButton}>Actividades</button>
@@ -13,10 +15,32 @@ function ActivityBar() {
 
         <div className={styles.notifContainer}>
           <p className={styles.toggleLabel}>No</p>
-          <input type="checkbox" id="check" className={styles.hiddenCheckbox} />
+          <input 
+            type="checkbox" 
+            id="check" 
+            className={styles.hiddenCheckbox} 
+            checked={notificationsEnabled}
+            onChange={() => setNotificationsEnabled(!notificationsEnabled)}
+          />
           <label htmlFor="check" className={styles.notifButton} />
           <p className={styles.toggleLabel}>Si</p>
         </div>
+
+        {notificationsEnabled && (
+          <>
+            <div className={styles.locationInputContainer}>
+              <input
+                type="text"
+                className={styles.locationInput}
+                placeholder="Indica la ubicación."
+              />
+            </div>
+
+            <button className={styles.submitButton}>
+              Guardar Cambios
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
